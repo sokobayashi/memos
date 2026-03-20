6ba20add75189edb5c8923050a1e5b3caaf7ddfe
SK <example@example.com>
2026-03-20 21:28:17 +0900

    Quartz sync: Mar 20, 2026, 9:28 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "So Kobayashi Notes",
+    pageTitle: "Messaboutwithtechno",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
