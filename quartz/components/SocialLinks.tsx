228f8af313d23c371d2ba674560af689bdb08ce3
SK <example@example.com>
2026-03-26 01:01:05 +0900

    Quartz sync: Mar 26, 2026, 1:01 AM

--- quartz/components/SocialLinks.tsx
+++ quartz/components/SocialLinks.tsx
@@ -1,28 +1,35 @@
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { joinSegments, pathToRoot } from "../util/path"
 import styles from "./styles/socialLinks.scss"
 
 export default (() => {
-  function SocialLinks(_props: QuartzComponentProps) {
+  function SocialLinks({ fileData }: QuartzComponentProps) {
+    const baseDir = fileData.slug === "404" ? "." : pathToRoot(fileData.slug!)
+    const bandcampIcon = joinSegments(baseDir, "static/bandcamp.svg")
+    const xIcon = joinSegments(baseDir, "static/x.svg")
+    const instagramIcon = joinSegments(baseDir, "static/instagram.svg")
+    const soundcloudIcon = joinSegments(baseDir, "static/soundcloud.svg")
+
     return (
       <div class="social-links">
         <a
-          href="https://sktmusic.bandcamp.com/"
+          href="https://skrtmusc.bandcamp.com/"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Bandcamp"
           title="Bandcamp"
         >
-          <img src="/static/bandcamp.svg" alt="Bandcamp" />
+          <img src={bandcampIcon} alt="Bandcamp" />
         </a>
 
         <a
-          href="https://x.com/sktechsktech"
+          href="https://x.com/sketchsketch"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="X"
           title="X"
         >
-          <img src="/static/x.svg" alt="X" />
+          <img src={xIcon} alt="X" />
         </a>
 
         <a
@@ -32,17 +39,17 @@ export default (() => {
           aria-label="Instagram"
           title="Instagram"
         >
-          <img src="/static/instagram.svg" alt="Instagram" />
+          <img src={instagramIcon} alt="Instagram" />
         </a>
 
         <a
-          href="https://soundcloud.com/sktechsktech"
+          href="https://soundcloud.com/sketchsketch"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="SoundCloud"
           title="SoundCloud"
         >
-          <img src="/static/soundcloud.svg" alt="SoundCloud" />
+          <img src={soundcloudIcon} alt="SoundCloud" />
         </a>
       </div>
     )
